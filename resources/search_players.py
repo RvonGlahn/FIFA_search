45437146bdc9a1b5a0bc68fe2d2f2a0275d252cb
RvonGlahn <rasmusvong@gmx.de>
2021-05-10 15:31:48 +0200

    test

--- resources/search_players.py
+++ resources/search_players.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import json
 import os
-from typing import List
 
 FILE_PATH = os.path.dirname(__file__)
 
@@ -17,7 +16,7 @@ class SearchPlayers:
         """
         Initializes dataframe for year 21.
         """
-        self.year = '21'
+        self.year = "21"
         self.df = pd.DataFrame()
         self.attributes = {}
 
@@ -37,7 +36,7 @@ class SearchPlayers:
         -------
         """
         self.year = year
-        data_path = os.path.join(FILE_PATH, 'data', 'players_' + year + '.csv')
+        data_path = os.path.join(FILE_PATH, "data", "players_" + year + ".csv")
         self.df = pd.read_csv(data_path, low_memory=False, encoding="utf-8")
 
     def _set_attributes(self):
@@ -47,14 +46,30 @@ class SearchPlayers:
         Returns
         -------
         """
+        # df.dtypes
         column_names = self.df.columns.to_list()
 
         self.attributes = {
-            "info": ["short_name", "age", "club_name", "overall", "potential", "value_eur", "wage_eur",
-                     "player_positons", "joined", "contract_valid_until", "pace", "shooting", "dribbling", "passing",
-                     "defending", "physic"],
-            'positions': list(map(str.upper, column_names[80:-1])),
-            'skills': column_names[39:79]
+            "info": [
+                "short_name",
+                "age",
+                "club_name",
+                "overall",
+                "potential",
+                "value_eur",
+                "wage_eur",
+                "player_positons",
+                "joined",
+                "contract_valid_until",
+                "pace",
+                "shooting",
+                "dribbling",
+                "passing",
+                "defending",
+                "physic",
+            ],
+            "positions": list(map(str.upper, column_names[80:-1])),
+            "skills": column_names[39:79],
         }
 
     def get_players(self, json_data: str) -> str:
@@ -74,20 +89,24 @@ class SearchPlayers:
 
         search_df = self.df
 
-        if req['name']:
-            search_df = search_df[search_df['name'].str.contains(req['name'])]
+        if req["name"]:
+            search_df = search_df[search_df["name"].str.contains(req["name"])]
 
-        if req['position']:
-            search_df = search_df[search_df['player_positons'] == req['position']]
+        if req["position"]:
+            search_df = search_df[search_df["player_positons"] == req["position"]]
 
-        if req['age']:
-            search_df = search_df[search_df.age >= req['age']]
+        if req["age"]:
+            search_df = search_df[search_df.age >= req["age"]]
 
-        if req['ability1name']:
-            search_df = search_df[search_df[req['ability1name']] >= int(req['ability1Value'])]
+        if req["ability1name"]:
+            search_df = search_df[
+                search_df[req["ability1name"]] >= int(req["ability1Value"])
+            ]
 
-        if req['ability2Name']:
-            search_df = search_df[search_df[req['ability2Name']] >= int(req['ability2Value'])]
+        if req["ability2Name"]:
+            search_df = search_df[
+                search_df[req["ability2Name"]] >= int(req["ability2Value"])
+            ]
 
         return search_df.to_json()
 
@@ -102,15 +121,20 @@ class SearchPlayers:
         return json.dumps(self.attributes)
 
     def get_suggestion(self, subname):
-        name_suggest = [name for name in self.df['Name'].to_list() if subname in name]
+        name_suggest = [name for name in self.df["Name"].to_list() if subname in name]
         return name_suggest
+
+    def suggest_similar_player(self):
+        pass
 
 
 if __name__ == "__main__":
-    req_json = '{ "name":"" , "position":"" , "age":30, "ability1Name":"POT", "ability1Value":80 , ' \
-               '"ability2Name":"OVA", "ability2Value":80}'
+    req_json = (
+        '{ "name":"" , "position":"" , "age":30, "ability1Name":"POT", "ability1Value":80 , '
+        '"ability2Name":"OVA", "ability2Value":80}'
+    )
 
     search = SearchPlayers()
     print(search.get_suggestion("Ronal"))
-    json = search.get_players(req_json)
-    print(json)
+    json_data = search.get_players(req_json)
+    print(json_data)
